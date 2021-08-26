4e02052307087ccc2783f5d54b439dc603ae26f2
Batya <Gigi.ucla21@gmail.com>
2021-08-25 17:07:58 -0700

    add fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an alert! JavaScript is running!");
+
+var playerName = window.prompt("What is your robots name?");
+console.log(playerName);
+console.log("This logs a string, good enough for leaving yourself a message");
+console.log(10 + 10);
+console.log("Our robots name is " + playerName);
+
+function fight() {
+  var playerName = window.alert("The fight has begun!");
+}
+// fight();
